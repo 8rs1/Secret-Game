e50919af990a6ba65b3d75049ea1d80752853947
LiY3R <rahimi98871@gmail.com>
2023-12-17 15:27:01 +0330

    add consol and janebi pictures in homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,31 @@
 <template>
-  <h1>homePage</h1>
-  
+  <h1>homePage</h1> 
+  <div class="pitures">
+    <div class="divpic">
+      <img src="../assets/console-banner.jpg" alt="consolbanner" class="pics">
+    </div>
+    <div class="divpic">
+      <img src="../assets/janebi-banner.jpg" alt="janebibanner" class="pics">
+    </div>
+  </div>
 </template>
+<style scoped>
+    .pitures {
+      height: 227px;
+      display: flex;
+
+    }
+    .divpic {
+      width: 50%;
+      height: 227px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .pics {
+      width: 600px;
+      height: 200px;
+      border-radius: 12px;
+    }
+
+</style>
