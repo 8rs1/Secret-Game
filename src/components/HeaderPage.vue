2ea2f2162c64f0d2ffbf7c3d0b7974265429737d
8rs1 <arssani.info@gmail.com>
2023-12-17 16:31:54 +0330

    add arrow & layout header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,13 +1,59 @@
 <template>
   <header class="header">
-    <div>logo</div>
-    <div>navbar</div>
-    <div>login/signup search</div>
+    <div class="logo-container">logo</div>
+    <div class="navigation-container">
+      <nav class="nav">
+        <ul class="navbar">
+          <li class="nav-item">
+          اخبار و مقالات
+          <i class="far fa-angle-down"></i>
+        </li>
+        <li class="nav-item">
+          ویدیو
+          <i class="far fa-angle-down"></i>
+        </li>
+        <li class="nav-item">
+          بررسی بازی&zwnj;ها
+          <i class="far fa-angle-down"></i>
+        </li>
+        <li class="nav-item">
+          آموزش
+          <i class="far fa-angle-down"></i>
+        </li>
+        <li class="nav-item">
+          سکرت گیم
+          <i class="far fa-angle-down"></i>
+        </li>
+        <li class="nav-item">
+          تبلیغات
+          <i class="far fa-angle-down"></i>
+        </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="actions-container">login/signup search</div>
   </header>
 </template>
 <style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
 .header {
   display: flex;
   justify-content: space-between;
+  background-color: #eaeaea;
+  padding: 1rem 2rem;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+}
+.nav-item {
+  display: flex;
+  gap: .2rem;
+  align-items: center;
 }
 </style>
