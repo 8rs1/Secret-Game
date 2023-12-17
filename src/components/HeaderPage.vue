d8e1e5a4ef5aa8527475a7d1d128499922c93567
8rs1 <arssani.info@gmail.com>
2023-12-17 16:00:48 +0330

    add header structure

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div>logo</div>
+    <div>navbar</div>
+    <div>login/signup search</div>
+  </header>
+</template>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
