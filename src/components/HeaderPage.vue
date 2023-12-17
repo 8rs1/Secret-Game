2fc4bf62068069ad180f16d56fb6adb4de15777b
8rs1 <arssani.info@gmail.com>
2023-12-17 20:08:21 +0330

    add drop-down menu and fix box-sizing

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -5,28 +5,68 @@
       <nav class="nav">
         <ul class="navbar">
           <li class="nav-item">
-          اخبار و مقالات
-          <i class="far fa-angle-down"></i>
-        </li>
-        <li class="nav-item">
-          ویدیو
-          <i class="far fa-angle-down"></i>
-        </li>
-        <li class="nav-item">
-          بررسی بازی&zwnj;ها
-          <i class="far fa-angle-down"></i>
-        </li>
-        <li class="nav-item">
-          آموزش
-          <i class="far fa-angle-down"></i>
-        </li>
-        <li class="nav-item">
-          سکرت گیم
-          <i class="far fa-angle-down"></i>
-        </li>
-        <li class="nav-item">
-          تبلیغات
-        </li>
+            اخبار و مقالات
+            <i class="far fa-angle-down"></i>
+            <ul class="submenu">
+              <li class="submenu-item">لورم1</li>
+              <li class="submenu-item">لورم2</li>
+              <li class="submenu-item">لورم3</li>
+              <li class="submenu-item">لورم4</li>
+              <li class="submenu-item">لورم5</li>
+              <li class="submenu-item">لورم6</li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            ویدیو
+            <i class="far fa-angle-down"></i>
+            <ul class="submenu">
+              <li class="submenu-item">لورم1</li>
+              <li class="submenu-item">لورم2</li>
+              <li class="submenu-item">لورم3</li>
+              <li class="submenu-item">لورم4</li>
+              <li class="submenu-item">لورم5</li>
+              <li class="submenu-item">لورم6</li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            بررسی بازی&zwnj;ها
+            <i class="far fa-angle-down"></i>
+            <ul class="submenu">
+              <li class="submenu-item">لورم1</li>
+              <li class="submenu-item">لورم2</li>
+              <li class="submenu-item">لورم3</li>
+              <li class="submenu-item">لورم4</li>
+              <li class="submenu-item">لورم5</li>
+              <li class="submenu-item">لورم6</li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            آموزش
+            <i class="far fa-angle-down"></i>
+            <ul class="submenu">
+              <li class="submenu-item">لورم1</li>
+              <li class="submenu-item">لورم2</li>
+              <li class="submenu-item">لورم3</li>
+              <li class="submenu-item">لورم4</li>
+              <li class="submenu-item">لورم5</li>
+              <li class="submenu-item">لورم6</li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            سکرت گیم
+            <i class="far fa-angle-down"></i>
+            <ul class="submenu">
+              <li class="submenu-item">لورم1</li>
+              <li class="submenu-item">لورم2</li>
+              <li class="submenu-item">لورم3</li>
+              <li class="submenu-item">لورم4</li>
+              <li class="submenu-item">لورم5</li>
+              <li class="submenu-item">لورم6</li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            تبلیغات
+          </li>
         </ul>
       </nav>
     </div>
@@ -55,5 +95,41 @@ ul {
   display: flex;
   gap: .2rem;
   align-items: center;
+  position: relative;
+  width: 8rem;
+  justify-content: center;
+  text-align: center;
+}
+.nav-item:hover .fa-angle-down::before {
+  content: "\f106";
+}
+.submenu {
+  position: absolute;
+  top: 10%;
+  box-shadow: 1px 1px 10px #aaa;
+  border-radius: 12px;
+  width: 100%;
+  visibility: hidden;
+  opacity: 0;
+  transition: all 100ms ease-out;
+  overflow: hidden;
+}
+.submenu-item {
+  background-color: #fff;
+  padding: .3rem;
+}
+.nav-item:hover>.submenu {
+  top: 100%;
+  visibility: visible;
+  opacity: 1;
+  animation: drop-down_animation 200ms ease 1 alternate;
+}
+@keyframes drop-down_animation {
+  0%,50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
