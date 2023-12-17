10e06c292752ac36b24783d86ef5d4a7ed5d908c
SkyDencer <jodiedwared8@gmail.com>
2023-12-17 15:10:14 +0330

    kmael shodan 4 ta icon poshtibani dar aval site

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,47 @@
 <template>
-  <h1>homePage</h1>
-  salam
+  <div class="main">
+    <div class="service">
+        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H216v32c0 13.3-10.7 24-24 24s-24-10.7-24-24V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h32V200z"></path></svg><span class="service-item-span">دنیای گیم</span></p>
+        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M256 80C149.9 80 62.4 159.4 49.6 262c9.4-3.8 19.6-6 30.4-6c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48c-44.2 0-80-35.8-80-80V384 336 288C0 146.6 114.6 32 256 32s256 114.6 256 256v48 48 16c0 44.2-35.8 80-80 80c-26.5 0-48-21.5-48-48V304c0-26.5 21.5-48 48-48c10.8 0 21 2.1 30.4 6C449.6 159.4 362.1 80 256 80z"></path></svg><span class="service-item-span">پشتیبانی عالی</span></p>
+        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg><span class="service-item-span">بازگشت وجه</span></p>
+        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg><span class="service-item-span">تحویل سریع</span> </p>
+      </div>
+  </div>
 </template>
+<style scoped>
+.main{
+        /* background-color: black; */
+        width: 100%;
+        height: auto;
+    }
+    .service{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        width: 100;
+        height: 100px;
+        font-size: 20px;
+    }
+    .service-item{
+        display: flex;
+        justify-content: space-around;
+        direction: rtl; 
+        width: 15%;
+        height: 60px;
+        border-style: solid;
+        border-color: #45494d;
+        border-radius: 1rem;
+        background-color: #d9eeff;
+    }
+    .service-item-icon{
+        width: 30px;
+        height: 60px;
+    }
+    .service-item-span{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+        line-height: 10px;
+    }
+</style>
