34fa5a58b489a76dc7f8671b43db0ac785472dc2
LiY3R <rahimi98871@gmail.com>
2023-12-17 16:42:40 +0330

    add newest products text in home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,10 +2,9 @@
   <h1>homePage</h1> 
   <div class="main">
     <div class="service">
-        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H216v32c0 13.3-10.7 24-24 24s-24-10.7-24-24V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h32V200z"></path></svg><span class="service-item-span">دنیای گیم</span></p>
-        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M256 80C149.9 80 62.4 159.4 49.6 262c9.4-3.8 19.6-6 30.4-6c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48c-44.2 0-80-35.8-80-80V384 336 288C0 146.6 114.6 32 256 32s256 114.6 256 256v48 48 16c0 44.2-35.8 80-80 80c-26.5 0-48-21.5-48-48V304c0-26.5 21.5-48 48-48c10.8 0 21 2.1 30.4 6C449.6 159.4 362.1 80 256 80z"></path></svg><span class="service-item-span">پشتیبانی عالی</span></p>
-        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg><span class="service-item-span">بازگشت وجه</span></p>
         <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg><span class="service-item-span">تحویل سریع</span> </p>
+        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg><span class="service-item-span">بازگشت وجه</span></p>
+        <p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M256 80C149.9 80 62.4 159.4 49.6 262c9.4-3.8 19.6-6 30.4-6c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48c-44.2 0-80-35.8-80-80V384 336 288C0 146.6 114.6 32 256 32s256 114.6 256 256v48 48 16c0 44.2-35.8 80-80 80c-26.5 0-48-21.5-48-48V304c0-26.5 21.5-48 48-48c10.8 0 21 2.1 30.4 6C449.6 159.4 362.1 80 256 80z"></path></svg><span class="service-item-span">پشتیبانی عالی</span></p><p class="service-item"><svg class="service-item-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><path d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H216v32c0 13.3-10.7 24-24 24s-24-10.7-24-24V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h32V200z"></path></svg><span class="service-item-span">دنیای گیم</span></p>
       </div>
   </div>
   <div class="pitures">
@@ -16,8 +15,23 @@
       <img src="../assets/janebi-banner.jpg" alt="janebibanner" class="pics">
     </div>
   </div>
+  <div class="newest-products-holder">
+    <div class="newest-products-border-bottom">
+      <div class="newest-products">
+        <p class="product-title"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M159.3 5.4c7.8-7.3 19.9-7.2 27.7 .1c27.6 25.9 53.5 53.8 77.7 84c11-14.4 23.5-30.1 37-42.9c7.9-7.4 20.1-7.4 28 .1c34.6 33 63.9 76.6 84.5 118c20.3 40.8 33.8 82.5 33.8 111.9C448 404.2 348.2 512 224 512C98.4 512 0 404.1 0 276.5c0-38.4 17.8-85.3 45.4-131.7C73.3 97.7 112.7 48.6 159.3 5.4zM225.7 416c25.3 0 47.7-7 68.8-21c42.1-29.4 53.4-88.2 28.1-134.4c-4.5-9-16-9.6-22.5-2l-25.2 29.3c-6.6 7.6-18.5 7.4-24.7-.5c-16.5-21-46-58.5-62.8-79.8c-6.3-8-18.3-8.1-24.7-.1c-33.8 42.5-50.8 69.3-50.8 99.4C112 375.4 162.6 416 225.7 416z"></path></svg> جدیدترین محصولات</p>
+      </div>
+      <div class="newest-products-txt">
+        <span> 
+          مشاهده همه
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
+*{
+  direction: rtl;
+}
 .main{
         /* background-color: black; */
         width: 100%;
@@ -56,7 +70,6 @@
     .pitures {
       height: 227px;
       display: flex;
-
     }
     .divpic {
       width: 50%;
@@ -69,6 +82,38 @@
       width: 600px;
       height: 200px;
       border-radius: 12px;
+    }
+    .newest-products-holder {
+      height: 50px;
+      display: flex;
+      display: flex;
+      align-items: center;
+    }
+    .newest-products-border-bottom {
+      width: 300px;
+      height: 40px;
+      border-bottom: 3px solid rgb(0, 110, 255);
+      display: flex;
+      align-items: center;
+    }
+    .newest-products {
+      width: 200px;
+      height:50px;
+      margin-right: 20px;
+      display: flex;
+      align-items: center;
+    }
+    .product-title {
+      font-size: 26px;
+    }
+    .newest-products-txt {
+      width: 80px;
+      height: 30px;
+      display: flex;
+      align-items: flex-start;
+      justify-content: center;
+      color: rgb(0, 89, 255);
+      font-weight: bold;
     }
 
 </style>
