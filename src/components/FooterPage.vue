30a4dcb218ee054d8c7b84b9ee66b83d354c14e2
8rs1 <arssani.info@gmail.com>
2023-12-21 12:51:34 +0330

    feat: add copyright text

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -2,12 +2,25 @@
 <template>
   <footer class="footer">
     <div>
-      <div>دسترسی سریع</div>
+      <div class="copy">
+        &copy; All right reserved for
+        <RouterLink to="/">Secret Game</RouterLink>
+        2023
+      </div>
     </div>
   </footer>
 </template>
 <style scoped>
+a {
+  font-weight: bold;
+}
 .footer {
   background-color: var(--first-color);
+  padding: 1rem 0;
+}
+.copy {
+  direction: ltr;
+  color: #ddd;
+  font-family: monospace;
 }
 </style>
