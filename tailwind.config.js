1e1bfb0cff30b3611bdc9d14535c6df588189648
8rs1 <arssani.info@gmail.com>
2023-12-21 14:47:37 +0330

    feat: change header styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ export default {
   content: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'dropdown_animation': 'drop-down_animation 200ms ease 1 alternate'
+      },
+      keyframes: {
+        dropdown_animation: {
+          '0%, 50%': {
+            opacity: 0
+          },
+          '100%': {
+            opacity: 1
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
