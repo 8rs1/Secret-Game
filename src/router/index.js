f610c9c84fe8bf075145c9d00be9ef66a1186415
LiY3R <rahimi98871@gmail.com>
2023-12-22 19:31:29 +0330

    add aboutpage branche and feat about in indexjs

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import AboutPage from '@/views/aboutpage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutPage
     },
   ]
 })
